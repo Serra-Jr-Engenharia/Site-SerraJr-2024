da53ab59c851a9a2e60c4dc435cc64a8834a9f55
Victor Hugo Almeida Rodrigues e Souza <arsvhugo@hotmail.com>
2024-11-12 02:29:39 -0300

    atualicação parte mecanica

--- src/Pages/Home/Components/Mecanica/style.ts
+++ src/Pages/Home/Components/Mecanica/style.ts
@@ -1,28 +1,48 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
+    display: flex;
+    justify-content: center;
 
 `;
 
 export const LeftContent = styled.div `
+    width: 50%;
 
 `;
 
 export const RightContent = styled.div `
+    margin-right: 100px;
+
 
 `;
 
 export const Title = styled.p `
+    font-size:36px;
+    color: #000D1A;
+    font-weight: 800;
+    margin: 20px 0;
 
 
 `;
 
 export const Text = styled.p `
+    color: #505050;
 
 
 `;
 
 export const Card = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.2);
+    width: 350px;
+    height: 60px;
+    margin: 20px 0px;
+    color: #000D19;
     
 
 
@@ -38,9 +58,30 @@ export const CardContainer = styled.div `
 `;
 
 export const Button = styled.button `
+    color: white;
+    background-color: #000D19;
+    border-radius: 10px;
+    width: 350px;
+    height: 50px;
+    font-size: 16px;
+    margin-top: 20px;
+    cursor: pointer;
 
 `;
 
 export const Image = styled.img `
+    margin-top: -100px;
+    width: 450px;
+
+`;
+
+export const Icon = styled.img `
+    width: 50px;
+    display: flex;
+    margin-right: 10px;
+    background-color: #000D19;
+    
+
+
 
 `;
