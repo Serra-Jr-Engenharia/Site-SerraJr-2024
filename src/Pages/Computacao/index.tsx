73f890b01810704cf80a6147770f0797c5c5049e
BryanDevil12 <bryan-devil12@hotmail.com>
2025-02-04 16:14:53 -0300

    adicionando 2 cards na carta de serviços

--- src/Pages/Computacao/index.tsx
+++ src/Pages/Computacao/index.tsx
@@ -1,54 +1,75 @@
-import { useState } from 'react'
-import Slider from './Components/Slider/index'
-import Banner from './Components/Banner/index'
-import {Title, TitleContainer, CardContent} from './style'
-import CardP from "../../Components/CardP"
-import cervejelas from "../../Assets/Cervejelas.png"
-import tmf from "../../Assets/TMF.png"
-import ods from "../../Assets/odsquiz 1.png"
-import prownovar from "../../Assets/prownovar.png"
-import etar from "../../Assets/etarserra 2023.png"
-import friburguense from "../../Assets/Friburguense.svg"
+import { useState } from 'react';
+import Slider from './Components/Slider/index';
+import Banner from './Components/Banner/index';
+import { Title, TitleContainer, CardContent, CardSpecialContainer, CardSpecial } from './style';
+import CardP from "../../Components/CardP";
+import cervejelas from "../../Assets/Cervejelas.png";
+import tmf from "../../Assets/TMF.png";
+import ods from "../../Assets/odsquiz 1.png";
+import prownovar from "../../Assets/prownovar.png";
+import etar from "../../Assets/etarserra 2023.png";
+import friburguense from "../../Assets/Friburguense.svg";
+import setinhal from "../../Assets/setinhal.png";
+import setinhar from "../../Assets/setinhar.png";
+import http from "../../Assets/http.png";
+import notebook from "../../Assets/notebook.png";
 
-const Computacao: React.FC = () =>{
-
+const Computacao: React.FC = () => {
     const [expandedCard, setExpandedCard] = useState(null);
 
     const handleCardClick = (index) => {
-        setExpandedCard( index === expandedCard ? null : index);
-    }
+        setExpandedCard(index === expandedCard ? null : index);
+    };
 
     const cardsData = [
-        { title: "Cervejelas", img: cervejelas,description: "Cervejelas 2023"},
-        {title: "ODS Quiz",img: ods,description: "Hackathon Hack2030"},
-        {title: "Projeto Friburguense", img: friburguense,description: "Friburguense Atlético Clube"},
-        {title: "Projeto TMF", img: tmf,description: "Cervejelas 2023"},
-        {title: "ETArserra 2022", img: etar,description: "Cervejelas 2023"},
-        {title: "Prownovar", img: prownovar,description: "Cervejelas 2023"},
+        { title: "Cervejelas", img: cervejelas, description: "Cervejelas 2023" },
+        { title: "ODS Quiz", img: ods, description: "Hackathon Hack2030" },
+        { title: "Projeto Friburguense", img: friburguense, description: "Friburguense Atlético Clube" },
+        { title: "Projeto TMF", img: tmf, description: "Cervejelas 2023" },
+        { title: "ETArserra 2022", img: etar, description: "Cervejelas 2023" },
+        { title: "Prownovar", img: prownovar, description: "Cervejelas 2023" },
     ];
 
-    return(
+    const cardsData2 = [
+        { title: "Desenvolvimento de Websites", img: http, description: "Desenvolvemos do zero um site pensado exclusivamente para atender as necessidades do seu negócio. Utilizamos das ferramentas mais modernas do mercado para criar uma solução única que irá alavancar a visibilidade do seu produto!" },
+        { title: "Desenvolvimento de Sistemas Web", img: notebook, description: "Com auxílio das mais completas linguagens de programação, fornecemos aplicativos e sistemas próprios para a sua empresa que irão acelerar e auxiliar internamente no gerenciamento e desenvolvimento do seu negócio." },
+    ];
+
+    return (
         <>
-            <Banner/>
-            <div style={{height: '60vh', width: '100%'}}>
-
-            <TitleContainer> 
-                <Title>
-                    Carta de serviços
-                </Title>
-                <hr/>
+        <Banner />
+        <div style={{ height: '30vh', width: '100%' }}>
+            <TitleContainer>
+            <Title>
+                Carta de serviços
+            </Title>
+            <hr />
             </TitleContainer>
+        </div>
 
-            </div>
-            <CardContent>
-                    {cardsData.map((card,index) => (
-                        <CardP key={index} title={card.title} img={card.img} description={card.description} isExpanded={expandedCard === index} onClick={() => handleCardClick(index)}/>
-                    ))}
-            </CardContent>
-            <Slider/>
-            
+        {/* Card isolado centralizado na tela */}
+        <CardSpecialContainer>
+            {cardsData2.map((card, index) => (
+                <CardSpecial key={index}>
+                <img src={card.img} alt={card.title} />
+                <p className="title-card">{card.title}</p>
+                <p className="description-card">{card.description}</p>
+                <img className="setinha" src={setinhal} alt="Seta esquerda" />
+                <img className="setinha" src={setinhar} alt="Seta direita" />
+                </CardSpecial>
+
+            ))}
+        </CardSpecialContainer>
+
+        <CardContent>
+            {cardsData.map((card, index) => (
+            <CardP key={index} title={card.title} img={card.img} description={card.description} isExpanded={expandedCard === index} onClick={() => handleCardClick(index)} />
+            ))}
+        </CardContent>
+
+        <Slider />
         </>
-    )
-}
+    );
+    };
 
-export default Computacao
+export default Computacao;
--- src/Pages/Computacao/style.ts
+++ src/Pages/Computacao/style.ts
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 
-
 export const Content = styled.div`
-
   color: white;
   display: flex;
   flex-direction: column;
@@ -20,16 +18,91 @@ export const TitleContainer = styled.div`
   padding: 40px;
   width: 100%;
   height: 200px;
-`
+`;
 
 export const CardContent = styled.div`
   display: flex;
   gap: 1rem;
   justify-content: center;
   align-items: center;
-`
+  flex-wrap: wrap;
+`;
 
 export const Title = styled.h2`
   color: "#000D1A";
   font-size: 3rem;
-`
+`;
+
+// Container de cardsData2(aulas)
+export const CardSpecialContainer = styled.div`
+  position: relative;  // Posiciona o card dentro do fluxo
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12rem;
+  gap: 1rem;
+`;
+
+export const CardSpecial = styled.div`
+  width: 500px;
+  height: 400px;
+  background: transparent;
+  border-radius: 20px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
+  transition: transform 0.3s ease-in-out, background 0.3s ease-in-out;  // Transição suave no hover
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  position: relative;
+
+  &:hover {
+    background: linear-gradient(to right bottom, #000D1A, #04ADD7);  // Gradiente invertido
+    transform: scale(1.01); 
+  }
+
+  img {
+    margin-top: 2rem;
+    padding: 1rem;
+    max-width: 17%;
+    border-radius: 5px;
+  }
+
+  p.title-card {
+    position: relative;
+    top: -9rem;
+    margin-left: 7.6rem;
+    color: black;
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+
+  p.description-card {
+    position: relative;
+    text-align: justify;
+    top: -9rem;
+    left: 7.6rem;
+    color: gray;
+    font-size: 1rem;
+    width: 335px;
+  }
+
+  &:hover p.title-card {
+    color: white;
+  }
+
+  &:hover p.description-card {
+    color: white;
+  }
+
+
+  // Imagem adicional setas
+  .setinha {
+    position: absolute;
+    margin-left: 2rem;
+    margin-top: 9rem;
+    width: 42px;
+    height: 42px;
+  }
+`;
