517eb34390754f588e42bd668f75d973c3fd81be
EduH3435 <luiseduardofrazao@gmail.com>
2025-04-10 00:40:22 -0300

    PaginaDeErro

--- src/Pages/ErrorPage/index.tsx
+++ src/Pages/ErrorPage/index.tsx
@@ -1,7 +1,16 @@
+import { Content, ErroTexto, Paragraph, ErrorContent } from './style'
+import erro from "../../Assets/erro.png";
+
 export default function ErroPage() {
     return (
         <div>
-            <p>Teste Error Page</p>
+            <Content>
+                <ErrorContent>
+                    <img src={erro}/>
+                    <ErroTexto>ERROR 404</ErroTexto>
+                    <Paragraph>Página não encontrada!</Paragraph>
+                </ErrorContent>
+            </Content>
         </div>
     );
 }
---
+++ src/Pages/ErrorPage/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 60px;
+  );
+`;
+
+export const ErrorContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 50px;
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+`
+
+export const ErroTexto = styled.div`
+  font-size: 50px;
+  color: #000D1A;
+  font-weight: 600;
+`
+export const Paragraph = styled.div`
+  margin: 0px;
+  color: #000D1A;
+  font-size: 18px;
+`;
