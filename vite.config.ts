dbc3ad9227853638d744fa53e06f6c8c5e6cad97
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2025-05-20 21:24:25 -0300

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'Site-SerraJr-2024',
   optimizeDeps: {
     include: ['swiper'],  
   },
